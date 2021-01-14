fbc567bc1547986777403d024feb02c80045f9a2
a4deel <aa4deel@gmail.com>
2021-01-14 17:35:46 +0500

    Fixed Port renaming issue

--- src/app.js
+++ src/app.js
@@ -96,6 +96,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(PORT, () => {
-    console.log('Server is up on port ' + PORT)
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
